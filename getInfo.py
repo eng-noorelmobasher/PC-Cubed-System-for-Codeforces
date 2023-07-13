ea8b1bc45d54bfe47c5f50b1bd5841a76df3f251
noor_elmobasher <96498648+eng-noorelmobasher@users.noreply.github.com>
2023-07-13 05:25:14 +0300

    Fixed allowed statements bug

--- getInfo.py
+++ getInfo.py
@@ -3,15 +3,25 @@ from bs4 import BeautifulSoup
 def getProblemNames(link,session):
 
     url = link
-    classPart = link[link.index("/group"):]+'/problem/'
+
     response = session.get(url)
 
     soup = BeautifulSoup(response.text, 'html.parser')
-
+    excluded = ["act bottom dark", "bottom dark right"]
     problem_names = []
+    final_problem_names = []
+    table = soup.find('table', 'problems')
+    for problem in table.find_all('td', _class = lambda x:x not in excluded):
+        problem_names.append(problem.text.strip())
+
+    letters = problem_names[::4]
+    names = problem_names[1::4]
+    for i in range(len(names)):
+        names[i] = names[i][:names[i].index('\n')].strip()
+
+    for i in range(len(letters)):
+        final_problem_names.append(letters[i])
+        final_problem_names.append(names[i])
 
 
-    for problem_link in soup.find_all('a', href=lambda x: x and classPart in x):
-        problem_names.append(problem_link.text.strip())
-
-    return problem_names
+    return final_problem_names
